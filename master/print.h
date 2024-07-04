3e6b1a39a03795bb9ef2f8a32786bebabbf0f81d
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-04 11:31:22 +0530

    print.h, mainP1.cpp: ITER 0

---
+++ master/print.h
@@ -0,0 +1,31 @@
+/*
+print.h
+sidb95
+bhatoresiddharth@gmail.com
+SKYKS
+04 July 2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+	str1 = "Hello World";
+	Print() {
+		this.FLAG = true;
+	}
+
+	Print() {
+		this.FLAG = false;
+	}
+
+	void printPublicVar() {
+		std::cout << str1 << endl;
+	}
+
+	void printString(string strA) {
+		std::cout << str1 << endl;
+	}
+};
