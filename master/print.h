9f593c72760fad2bf6ef6a9e68fe46f8e6e69d0d
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-05 08:12:13 +0530

    master: Experiment 2

---
+++ master/print.h
@@ -0,0 +1,29 @@
+/*
+print.h
+sidb95
+bhatoresiddharth@gmail.com
+SKYKS
+04 July 2024
+*/
+
+#include "print.h"
+
+using namespace std;
+
+class Print {
+public:
+	str1 = "Hello World";
+	Print() {
+		this.FLAG = true;
+	}
+
+	Print() {
+		this.FLAG = false;
+	}
+
+	void printString(string strA) {
+		Print P1;
+		P1.printString(str1);
+		P1.printString(strA);
+	}
+};
