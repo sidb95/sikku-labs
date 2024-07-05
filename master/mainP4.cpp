ef84f670b985ce7f87e15ece666cade297e6b96b
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-05 09:11:38 +0530

    Create mainP4.cpp,mainP5.cpp: Experiment 2

---
+++ master/mainP4.cpp
@@ -0,0 +1,42 @@
+/*
+mainP4.cpp
+Main File 4
+sidb95
+bhatoresiddharth@gmail.com
+SKYKS
+*/
+
+#include "print.h"
+
+class Solution {
+    Solution() {
+        this.FLAG = true;
+    }
+
+    ~Solution() {
+        this.FLAG = false;
+    }
+
+    void mainP5Aux(string strA, int size1, int i) {
+        if (i == -1) {
+            return;
+        }
+        P1 = Print();
+        P1.printString(strA[i]);
+        mainP5Aux(strA, size1, i - 1);
+        return;
+    }
+
+    void mainP5(string strA) {
+        int size1 = strA.size();
+        mainP5Aux(strA, size1, size1 - 1);
+        return;
+    }
+};
+
+int main() {
+    Solution S1;
+    string str1 = "Hello World";
+    S1.mainP5(str1);
+    return 0;
+}
