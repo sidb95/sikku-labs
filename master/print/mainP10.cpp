f954fae2baf3fcc9f3c704cf8dab797837f863c7
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-10 19:05:01 +0530

    Update print

---
+++ master/print/mainP10.cpp
@@ -0,0 +1,48 @@
+/*
+Main File 10
+sidb95
+bhatoresiddharth@gmail.com 
+10 July 2024
+*/
+
+#include "print.h"
+#include <string>
+#include <set>
+
+class Solution {
+public:
+    FLAG = false;
+
+    Solution() {
+        FLAG = true;
+    }
+
+    ~Solution() {
+        FLAG = false;
+    }
+
+    void printStringAux(set <string>& S) {
+        set <string>::iterator i;
+        for (i = S.begin(); i != S.end(); i++) {
+        printString(*i);
+        printString(' ');
+     }
+        return;
+    }
+
+    void printString(string strA) {
+        size1 = strA.size();
+        set <string> S1;
+        int itr = 0;
+        while (itr != size1) {
+            string str1 = "";
+            while (strA[itr] != ' ') {
+                str1 += strA[i];
+            }
+            S1.insert(str1);
+            itr += 1;
+        }
+        printStringAux(S1);
+        return
+    }
+};
