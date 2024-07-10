bb575a788d19e4184a689c74f2baa919eac82db0
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-10 16:59:59 +0530

    Update mainP9.cpp

--- master/print/mainP9.cpp
+++ master/print/mainP9.cpp
@@ -1,1 +1,25 @@
+#include "print.h"
+#include <string>
 
+class Solution {
+public:
+    FLAG = false;
+
+    Solution() {
+        FLAG = true;
+    }
+
+    ~Solution() {
+        FLAG = false;
+    }
+
+    void mainP9(string strA) {
+        size1 = strA.size();
+        Print P1;
+        for (int i = 0; i < size1; i += 1) {
+            P1.printFunc(strA[i]);
+        }
+        P1.printFunc('\n');
+        return;
+    }
+};
