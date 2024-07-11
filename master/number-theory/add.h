729686dbb397d45108a78989974594a4816a19a2
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 11:09:47 +0530

    Update add.h

--- master/number-theory/add.h
+++ master/number-theory/add.h
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+class Numbers {
+public:
+    FLAG = false;
+
+    Numbers() {
+        FLAG = true;
+    }
+
+    ~Numbers() {
+        FLAG = false;
+    }
+
+    int sumTwoNums(int a, int b) {
+        return (a + b)
+    }
+};
