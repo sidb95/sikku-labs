a3cb962880fcbf0404d9e43b654a07985e335929
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 11:21:39 +0530

    Update gcd.h

--- master/number-theory/gcd.h
+++ master/number-theory/gcd.h
@@ -1,1 +1,18 @@
+/*
+gcd.h
+sidb95 
+bhatoresiddharth@gmail.com 
+11 July 2024
+*/
 
+int gcd(int a, int b) {
+    if (a == 1) {
+        return b;
+    }
+    else {
+        int TEMP = a;
+        a = b;
+        b = TEMP % b;
+        return gcd(a, b);
+    }
+}
