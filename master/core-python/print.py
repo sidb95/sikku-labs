8f74e7530d7dd39dc90ce958b8e1e2c501c50acf
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-12 16:36:40 +0530

    Update print.py

--- master/core-python/print.py
+++ master/core-python/print.py
@@ -4,3 +4,16 @@ sidb95
 bhatoresiddharth@gmail.com
 12 July 2024
 """
+
+class Print:
+  def __init__(self):
+    pass
+
+  def printString(self, s: str) ->:
+    print(s)
+    return
+
+  def printCharacter(self, c: char) ->:
+    print(c, end = "")
+    return
+  
