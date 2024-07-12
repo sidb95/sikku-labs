fb073731207006757f61e13a128ee10d52e617c4
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-12 16:42:37 +0530

    Update mainP1.py

--- master/core-python/mainP1.py
+++ master/core-python/mainP1.py
@@ -4,3 +4,13 @@ sidb95
 bhatoresiddharth@gmail.com 
 12 July 2024
 """
+from print import Print
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def mainP1(self, s: str) -> None:
+    P1 = Print()
+    P1.printString(s)
+    return None
