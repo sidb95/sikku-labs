a8aa804cf5d6b9eb7ccdebba93973f4063c1ade1
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-12 06:40:57 +0530

    Update isInteger1.h

--- master/number-theory/isInteger1.h
+++ master/number-theory/isInteger1.h
@@ -4,3 +4,19 @@ sidb95
 bhatoresiddharth@gmail.com
 12 July 2024
 */
+#include <string>
+
+class Solution {
+public:
+    bool isInteger1(double a) {
+        string b = to_string(a);
+        bool propn1 = true;
+        for (int i = 0; i < b.size(); i += 1) {
+            if (b[i] == '.') {
+                propn1 = false;
+            }
+        }
+        return propn1;
+        }
+    }
+};
