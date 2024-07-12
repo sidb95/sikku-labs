2b03793a01cb45ae4ee8559baf3945bc97e648fd
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-12 16:47:50 +0530

    Update mainP2.py

--- master/core-python/mainP2.py
+++ master/core-python/mainP2.py
@@ -4,3 +4,17 @@ sidb95
 bhatoresiddharth@gmail.com
 12 July 2024
 """
+
+from print import Print
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def mainP2(self, s: str) -> None:
+    P1 = Print()
+    size1 = len(s)
+    for i in range(0, size1):
+      P1.printCharacter(s[i])
+    P1.printCharacter('\n')
+    return None
