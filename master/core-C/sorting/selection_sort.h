50f72f8a9c1ec87d2e29e6e0783d0150a1a6c129
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-14 11:58:52 +0530

    Update selection_sort.h

--- master/core-C/sorting/selection_sort.h
+++ master/core-C/sorting/selection_sort.h
@@ -4,3 +4,19 @@ sidb95
 bhatoresiddharth@gmail.com 
 14 July 2024
 */
+
+int* selectionSort(int* nums, int n) {
+    int ptr, itr;
+    while (ptr >= 0) {
+        ptr = n - 1;
+        for (int i = 0; i < ptr; i += 1) {
+            if (nums[i] > nums[ptr]) {
+                int TEMP = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = TEMP;
+            }
+        }
+        ptr += 1;
+    }
+    return nums;
+}
