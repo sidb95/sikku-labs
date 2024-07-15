7d421110ccc352aab7c562ca00d8caf1c547a696
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-15 10:21:35 +0530

    Update print.h

--- master/core-C/print/print.h
+++ master/core-C/print/print.h
@@ -4,3 +4,19 @@ sidb95
 bhatoresiddharth@gmail.com 
 15 July 2024
 */
+
+#include <stdio.h>
+
+void printString(char* s);
+
+void printString(char* s) {
+    int size1 = 0, i;
+    while (s[size1] != '\0') {
+        size1 += 1;
+    }
+    for (i = 0; i < size1; i += 1) {
+        printf("%c", s[i]);
+    }
+    printf('\n');
+    return;
+}
