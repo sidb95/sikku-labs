c28a55e6b7c992ec966e03ad9c4ff797e119e32f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-16 07:12:33 +0530

    Update mainP11.cpp

--- master/print/mainP11.cpp
+++ master/print/mainP11.cpp
@@ -1,6 +1,20 @@
 /*
-Main File 11
+Main File Print 11
 sidb95
 bhatoresiddharth@gmail.com
 11 July 2024
 */
+
+#include <string>
+#include <print.h>
+
+class Solution {
+private:
+    string str1 = "Hello World";
+public:
+    void mainP11(string str1) {
+        Print P1;
+        P1.printString(str1);
+        return;
+    }
+};
