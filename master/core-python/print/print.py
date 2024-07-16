ae0f6f742b5208123385b5acbb9e62fb0d81ab0a
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-16 15:53:45 +0530

    print.py: Experiment 1 Iter 2

--- master/core-python/print/print.py
+++ master/core-python/print/print.py
@@ -9,11 +9,13 @@ class Print:
   def __init__(self):
     pass
 
-  def printString(self, s: str) ->:
+  def printString(self, s: str) -> None:
     print(s)
     return
 
-  def printCharacter(self, c: char) ->:
+  def printCharacter(self, c: char) -> None:
     print(c, end = "")
     return
   
+  def lengthString(self, s: str) -> int:
+    return len(s)
