2251e8eef415f777318e9fbaa6ed40700335ce8b
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-16 17:41:00 +0530

    benchmarkP2: Exp 1 Iter 2

---
+++ master/core-python/print/benchmarkP2.py
@@ -0,0 +1,15 @@
+import time
+from mainP2 import *
+
+
+str1 = "nrhyos tov vn ilg hyt dexib wxei mr np uzmimi zstuqs mcxttnz hsxi qzxzyry hsd xbl ipc kfcr pkqqjb pcfa dqifx oosk dp aed ebvwq kpqa mhebn lm yg cx qm fvskp oizgda yac ptqaly eemymud nrsqnd kzqsyq gtiow tojd yi rgflt th abcxzpj jdwwlkw jvp pg lmhbmf oot vfhmdk ur yahvfg gx xhc wqr sxwztz nvoxegc ckkig azeg sr rncsx haz fpknti mlwew aif aiu nj uh jaabv qvradk hujv cqzkfh gz muwwi ltoy gczix tlrkvkk cbc hwehxag vwzfs ghx guiqs defajh fh ned aurkeuf fzzyzpt gkur op rirpn srcps zq et rfxesnf yssm qs lrla ca igltzl tbncdq bywcpw xqoh tsbhqjf jjwea xskgk mnxems ho lyddcpp pnbi dliv ela hsznej fp aekb iwbwbzi xe tzr njeq zhoofn czvh ifg leslrka agsjuw pn "
+
+
+def __main__():
+  S1 = Solution()
+  start = time.time()
+  S1.mainP2(str1)
+  end = time.time()
+  print(end - start)
+
+__main__()
