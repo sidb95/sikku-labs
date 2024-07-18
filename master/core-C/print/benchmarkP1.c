a66ab7c888f97680d04685633702305b8e132c22
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-18 14:00:46 +0530

    Update benchmarkP1.c

--- master/core-C/print/benchmarkP1.c
+++ master/core-C/print/benchmarkP1.c
@@ -4,3 +4,27 @@ sidb95
 bhatoresiddharth@gmail.com 
 18 July 2024
 */
+
+#include <stdio.h>
+#include <time.h>
+
+void printString(char* s);
+
+char* s;
+
+void printString(char* s) {
+    int size1 = 0, i;
+    while (s[size1] != '\0') {
+        size1 += 1;
+    }
+    for (i = 0; i < size1; i += 1) {
+        printf("%c", s[i]);
+    }
+    printf('\n');
+    return;
+}
+
+int main() {
+    printString(s);
+    return 0;
+}
