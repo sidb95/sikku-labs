ca41a37cc986b9d8ea2dd02ecb8d29416c49db78
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-18 16:02:19 +0530

    benchmarkP1.c

--- master/core-C/print/benchmarkP1.c
+++ master/core-C/print/benchmarkP1.c
@@ -10,7 +10,7 @@ bhatoresiddharth@gmail.com
 
 void printString(char* s);
 
-char* s;
+char s[20000] = {'\0'};
 
 void printString(char* s) {
     int size1 = 0, i;
@@ -20,11 +20,16 @@ void printString(char* s) {
     for (i = 0; i < size1; i += 1) {
         printf("%c", s[i]);
     }
-    printf('\n');
+    printf("\n");
     return;
 }
 
 int main() {
+    clock_t t; 
+    t = clock();
     printString(s);
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    printf("%f", time_taken);
     return 0;
 }
