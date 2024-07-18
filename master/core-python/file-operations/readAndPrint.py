72067578d45fa357505c3b0b829e12566f274409
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-18 13:34:35 +0530

    Update file-operations

---
+++ master/core-python/file-operations/readAndPrint.py
@@ -0,0 +1,19 @@
+"""
+readAndPrint.py
+sidb95
+bhatoresiddharth@gmail.com 
+18 July 2024
+"""
+
+class Solution:
+  def __init__(self)
+    pass
+
+  def readAndPrint(self, filename):
+    f = open(filename, 'r')
+    line = f.readline()
+    while (line != ""):
+      print(line, end = "")
+      line = f.readline()
+    print('\n')
+    return
