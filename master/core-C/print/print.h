5ea501680e2bf7f2f54d3d84365d940a90d37c40
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-18 14:09:39 +0530

    Update print.h

--- master/core-C/print/print.h
+++ master/core-C/print/print.h
@@ -8,6 +8,8 @@ bhatoresiddharth@gmail.com
 #include <stdio.h>
 
 void printString(char* s);
+void printCharacter(char c);
+void printStringRecursive(char* s, int n, int i);
 
 void printString(char* s) {
     int size1 = 0, i;
@@ -15,10 +17,21 @@ void printString(char* s) {
         size1 += 1;
     }
     for (i = 0; i < size1; i += 1) {
-        printf("%c", s[i]);
+        printCharacter(s[i]);
     }
     printf('\n');
     return;
+}
+
+void printStringRecursive(char* s, int n, int i) {
+    if (i == n) {
+        printf("\n");
+    }
+    else {
+        printCharacter(s[i]);
+        printStringRecursive(s, n, i + 1);
+        return;
+    }
 }
 
 void printCharacter(char c) {
