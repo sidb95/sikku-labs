f33a54f3b666624d6c516586c0c26877832b4724
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-19 13:54:42 +0530

    readAndPrint.py: core-python/file-opertions

--- master/core-python/file-operations/readAndPrint.py
+++ master/core-python/file-operations/readAndPrint.py
@@ -4,9 +4,10 @@ sidb95
 bhatoresiddharth@gmail.com 
 18 July 2024
 """
+import time
 
 class Solution:
-  def __init__(self)
+  def __init__(self):
     pass
 
   def readAndPrint(self, filename):
@@ -17,3 +18,13 @@ class Solution:
       line = f.readline()
     print('\n')
     return
+
+def __main__():
+  S = Solution()
+  start = time.time()
+  S.readAndPrint("gibberish_1.txt")
+  end = time.time()
+  print(end - start)
+  return
+
+__main__()
