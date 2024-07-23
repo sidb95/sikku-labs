0a2c8647c96d6544f42ab6b979149326183fd87c
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-23 17:45:24 +0530

    Update merge_sort.py

--- master/core-python/sorting/merge_sort.py
+++ master/core-python/sorting/merge_sort.py
@@ -4,3 +4,13 @@ sidb95
 bhatoresiddharth@gmail.com 
 23 July 2024
 """
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def mergeSortAux(self, arr: List[int], n: int, i: int, j: int) -> List[int]:
+    pass
+
+  def mergeSort(self, arr: List[int], n: i t, i: int, j: int) -> List[int]:
+    pass
