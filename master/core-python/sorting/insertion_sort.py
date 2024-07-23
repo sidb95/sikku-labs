a0b221f3101d019b1767d60bd4a5e75eff952ef4
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-23 12:57:04 +0530

    Update insertion_sort.py

--- master/core-python/sorting/insertion_sort.py
+++ master/core-python/sorting/insertion_sort.py
@@ -4,3 +4,14 @@ sidb95
 bhatoresiddharth@gmail.com
 23 July 2024
 """
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def insertionSort(self, nums: List[int]) -> List[int]:
+    n = len(nums)
+    for i in range(0, n):
+      for j in range(0, n):
+        if (nums[i] < nums[j]):
+          pass
