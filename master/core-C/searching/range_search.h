7c36c03c250836d3bbb8bd399cb6dc86d6e90275
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-28 17:12:58 +0530

    Update range_search.h

--- master/core-C/searching/range_search.h
+++ master/core-C/searching/range_search.h
@@ -4,3 +4,26 @@ sidb95
 bhatoresiddharth@gmail.com 
 28 July 2024
 */
+
+#include <stdlib.h>
+
+int range_search(int* nums, int n, int k) {
+    int p = 0;
+    int q = 0;
+    int num1 = 0;
+    while ((num1 + q) < k) {
+        p += 1;
+        q = pow(2, p - 1);
+        if ((num1 + q) > k) {
+            num1 += (q / 2)
+            p = 0;
+            q = 0;
+        }
+    }
+    if ((num1 + q) == k) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
