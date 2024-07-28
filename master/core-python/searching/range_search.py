587c3f448c1d0fe9a78cdbf58553503d87a3ed86
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-28 15:27:58 +0530

    Update range_search.py

--- master/core-python/searching/range_search.py
+++ master/core-python/searching/range_search.py
@@ -4,3 +4,12 @@ sidb95
 bhatoresiddharth@gmail.com
 28 July 2024
 """
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def rangeSearch(self, nums: List[int], k: int):
+    pass
+      
+    
