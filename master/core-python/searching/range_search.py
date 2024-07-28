ccdd6a57ba580216a72ed5f764d52f7bf51ad937
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-28 16:38:45 +0530

    Update range_search.py

--- master/core-python/searching/range_search.py
+++ master/core-python/searching/range_search.py
@@ -9,7 +9,20 @@ class Solution:
   def __init__(self):
     pass
 
-  def rangeSearch(self, nums: List[int], k: int):
-    pass
-      
+  def rangeSearch(self, nums: List[int], k: int) -> bool:
+    num1 = 0
+    p = 0
+    q = 0
+    while ((num1 + q) < k)):
+      p += 1
+      q = 2 ** (p - 1)
+      if ((num1 + q) > k)):
+        num1 += (q / 2)
+        q = 0
+        p = 0
+    if ((num1 + q) == k):
+      return True
+    else:
+      return False
+
     
