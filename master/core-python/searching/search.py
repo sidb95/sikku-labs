5021d15ce1269b1d91480c7fd71305c91acd68ff
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-28 16:42:01 +0530

    Update search.py

--- master/core-python/searching/search.py
+++ master/core-python/searching/search.py
@@ -4,3 +4,11 @@ sidb95
 bhatoresiddharth@gmail.com 
 28 July 2024
 """
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def searchArray(self, nums: List[int], k: int) -> bool:
+    return (k in nums)
+
