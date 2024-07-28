1154c1ef3046b98ac87e11b969c54097c311a955
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-28 21:03:18 +0530

    Update searchArray.h

--- master/core-cpp/searching/searchArray.h
+++ master/core-cpp/searching/searchArray.h
@@ -4,3 +4,16 @@ sidb95
 bhatoresiddharth@gmail.com 
 28 July 2024
 */
+
+bool searchArray(vector <int>& nums, int n, int k);
+
+bool searchArray(vector <int>& nums, int n, int k) {
+    bool propn1 = false;
+    for (int i = 0; i < n; i += 1) {
+        if (nums[i] == k) {
+            propn1 = true;
+            break;
+        }
+    }
+    return propn1;
+}
